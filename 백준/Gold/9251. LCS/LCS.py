145489abd528f5d81ec18d9dcae464c12f0121f3
ssafyswh <ssafyswh@gmail.com>
2025-10-15 22:47:38 +0900

    [Gold V] Title: LCS, Time: 432 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,19 +1,11 @@
 s1 = input()
 s2 = input()
-lcs = [[0] * len(s2) for _ in range(len(s1))]
-for y in range(len(s1)):
-    for x in range(len(s2)):
-        if s1[y] == s2[x]:
-            if x and y:
-                temp = lcs[y - 1][x - 1] + 1
-            else:
-                temp = 1
+lcs = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+# lcs[i][j]: s1[:i], s2[:j] 사이의 lcs
+for y in range(1, len(s1) + 1):
+    for x in range(1, len(s2) + 1):
+        if s1[y - 1] == s2[x - 1]:
+            lcs[y][x] = lcs[y - 1][x - 1] + 1
         else:
-            a, b = 0, 0
-            if x:
-                a = lcs[y][x - 1]
-            if y:
-                b = lcs[y - 1][x]
-            temp = max(a, b)
-        lcs[y][x] = temp
+            lcs[y][x] = max(lcs[y - 1][x], lcs[y][x - 1])
 print(lcs[-1][-1])

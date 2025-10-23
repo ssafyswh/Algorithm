2a00c682128f0c98c6baef70598beb6db55732e6
ssafyswh <ssafyswh@gmail.com>
2025-10-23 23:31:30 +0900

    [Gold V] Title: 용액, Time: 116 ms, Memory: 44748 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,25 @@
+import sys
+
+N = int(input())
+solutions = list(map(int, sys.stdin.readline().split()))
+front, rear = 0, N - 1
+a, b = solutions[front], solutions[rear]
+now = a + b
+result = now
+while front + 1 < rear:
+    if now > 0:
+        rear -= 1
+        now = solutions[front] + solutions[rear]
+        if abs(now) < abs(result):
+            a, b = solutions[front], solutions[rear]
+            result = now
+    elif now < 0:
+        front += 1
+        now = solutions[front] + solutions[rear]
+        if abs(now) < abs(result):
+            a, b = solutions[front], solutions[rear]
+            result = now
+    else:
+        break
+
+print(a, b)

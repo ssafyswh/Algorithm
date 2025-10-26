e4df6d2ec341ece3efe31b7e1a17b05889b0c2f9
ssafyswh <ssafyswh@gmail.com>
2025-10-26 22:53:39 +0900

    [Gold IV] Title: LCS 2, Time: 452 ms, Memory: 56604 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -3,22 +3,31 @@ len_s1 = len(s1)
 s2 = input()
 len_s2 = len(s2)
 
-lcs_length = [[0] * (len_s2 + 1) for _ in range(len_s1 + 1)]
-lcs_text = [[''] * (len_s2 + 1) for _ in range(len_s1 + 1)]
+lcs = [[0] * (len_s2 + 1) for _ in range(len_s1 + 1)]
 
 for y in range(1, len_s1 + 1):
     for x in range(1, len_s2 + 1):
         if s1[y-1] == s2[x-1]:
-            lcs_length[y][x] = lcs_length[y-1][x-1] + 1
-            lcs_text[y][x] = lcs_text[y-1][x-1] + s1[y-1]
+            lcs[y][x] = lcs[y - 1][x - 1] + 1
         else:
-            if lcs_length[y-1][x] > lcs_length[y][x-1]:
-                lcs_length[y][x] = lcs_length[y-1][x]
-                lcs_text[y][x] = lcs_text[y-1][x]
+            if lcs[y - 1][x] > lcs[y][x - 1]:
+                lcs[y][x] = lcs[y - 1][x]
             else:
-                lcs_length[y][x] = lcs_length[y][x-1]
-                lcs_text[y][x] = lcs_text[y][x-1]
+                lcs[y][x] = lcs[y][x - 1]
 
-print(lcs_length[len_s1][len_s2])
-if lcs_length[len_s1][len_s2]:
-    print(lcs_text[len_s1][len_s2])
+result_text = []
+y, x = len_s1, len_s2
+while y > 0 and x > 0:
+    if s1[y-1] == s2[x-1]:
+        result_text.append(s1[y-1])
+        y -= 1
+        x -= 1
+    elif lcs[y-1][x] > lcs[y][x-1]:
+        y -= 1
+    else:
+        x -= 1
+
+print(len(result_text))
+if len(result_text):
+    result_text.reverse()
+    print(''.join(result_text))

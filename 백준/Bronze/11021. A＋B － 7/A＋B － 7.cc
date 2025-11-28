5f6de39989e6d5e6959b528b2a550bb40e604bba
ssafyswh <ssafyswh@gmail.com>
2025-11-29 01:22:51 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T; 
+    cin >> T;
+    for (int case_num = 1; case_num <= T; ++case_num) {
+        int a, b;
+        cin >> a >> b;
+
+        int sum = a + b;
+        
+        cout << "Case #" << case_num << ": " << sum << "\n";
+    }
+
+    return 0;
+}

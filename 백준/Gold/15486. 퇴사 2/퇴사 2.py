df38cae44fe8bc580787d6c0ca3dfa1b584e00b2
ssafyswh <ssafyswh@gmail.com>
2025-12-22 00:13:03 +0900

    [Gold V] Title: 퇴사 2, Time: 1952 ms, Memory: 208496 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,13 +1,18 @@
 import sys
-input = sys.stdin.readline
+data = sys.stdin.read().split()
 
-N = int(input())
-
+N = int(data[0])
 dp = [0] * (N + 2)
+idx = 1
+
 for i in range(1, N + 1):
-    t, p = map(int, input().split())
-    dp[i] = max(dp[i], dp[i - 1])
-    if i + t <= N + 1:
-        dp[i + t] = max(dp[i + t], dp[i] + p)
+    t = int(data[idx])
+    p = int(data[idx + 1])
+    idx += 2
+
+    if dp[i] < dp[i - 1]:
+        dp[i] = dp[i - 1]
+    if i + t <= N + 1 and dp[i + t] < dp[i] + p:
+        dp[i + t] = dp[i] + p
 
 print(max(dp[-1], dp[-2]))

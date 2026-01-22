72f104f6902039d5580bd188a5687fc749234054
ssafyswh <ssafyswh@gmail.com>
2026-01-22 09:13:40 +0900

    [Silver II] Title: 이항 계수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
@@ -6,14 +6,13 @@ using namespace std;
 int main() {
     int N, K;
     cin >> N >> K;
-    vector<vector<int>> pascal(N + 1, vector<int>(N + 1));
-    for (int i = 0; i < N + 1; i++) {
-        pascal[i][0] = 1;
-        for (int j = 1; j < i; j++) {
-            pascal[i][j] = (pascal[i - 1][j - 1] + pascal[i - 1][j]) % 10007;
+    vector<int> dp(K + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= N; i++) {
+        for (int j = min(i, K); j > 0; j--) {
+            dp[j] = (dp[j] + dp[j - 1]) % 10007;
         }
-        pascal[i][i] = 1;
     }
-    cout << pascal[N][K] % 10007;
+    cout << dp[K];
     return 0;
 }

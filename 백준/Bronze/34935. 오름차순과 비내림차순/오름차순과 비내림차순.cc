59e339920f6e2754ccf0a3d503a874dccf95d446
ssafyswh <ssafyswh@gmail.com>
2026-04-08 09:07:08 +0900

    [Bronze IV] Title: 오름차순과 비내림차순, Time: 956 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34935. 오름차순과 비내림차순/오름차순과 비내림차순.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int result = 1;
+    long long num;
+    cin >> num;
+    for (int i = 1; i < N; i++) {
+        long long nxt;
+        cin >> nxt;
+        if (num >= nxt) result = 0;
+        num = nxt;
+    }
+    cout << result;
+    return 0;
+}

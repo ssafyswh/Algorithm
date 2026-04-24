7f91b15752f07cb80d521f18c6ae96a74efd077b
ssafyswh <ssafyswh@gmail.com>
2026-04-24 23:45:45 +0900

    [Silver III] Title: permutation making, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/22952. permutation making/permutation making.cc
+++ 백준/Silver/22952. permutation making/permutation making.cc
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
     int N;
     cin >> N;
-    for (int i = 0; i < (N - 1) / 2; i++) cout << i + 1 << " " << N - i - 1 << " ";
-    if (N % 2 == 0) cout << N / 2 << " ";
-    cout << N;
+
+    cout << N << " ";
+    for (int i = 1; i <= (N - 1) / 2; i++) {
+        cout << i << " " << N - i << " ";
+    }
+    if (N > 1 && N % 2 == 0) {
+        cout << N / 2;
+    }
     return 0;
 }
